cd86c7dfac21f5ea3dc2baccb82ec28d1f19a026
Aneli Valdés <aneli.valdes@thoughtworks.com>
2022-03-09 20:04:18 -0300

    refactor | improved logs

--- src/outbox/outbox.service.ts
+++ src/outbox/outbox.service.ts
@@ -1,8 +1,11 @@
-import { Inject, Injectable } from "@nestjs/common";
-import { OUTBOX_REPOSITORY, PENDING, SEQUELIZE } from "../constants";
-import { Outbox } from "./outbox.entity";
-import { EventPublisherService } from "../event-publisher/event-publisher.service";
-import { OrderEvent, OrderEventsTypes } from "../event-publisher/models/events.model";
+import { Inject, Injectable } from '@nestjs/common';
+import { OUTBOX_REPOSITORY, PENDING, SEQUELIZE } from '../constants';
+import { Outbox } from './outbox.entity';
+import { EventPublisherService } from '../event-publisher/event-publisher.service';
+import {
+  OrderEvent,
+  OrderEventsTypes,
+} from '../event-publisher/models/events.model';
 
 @Injectable()
 export class OutboxService {
@@ -11,37 +14,41 @@ export class OutboxService {
   constructor(
     @Inject(OUTBOX_REPOSITORY) private readonly outboxRepository: typeof Outbox,
     @Inject(SEQUELIZE) private readonly sequelize,
-    private readonly eventPublisher: EventPublisherService
-  ) {
-  }
+    private readonly eventPublisher: EventPublisherService,
+  ) {}
 
   public async poll(): Promise<void> {
     setInterval(() => {
-      console.log("----- Polling Outbox table from Order DB -------");
+      console.info(
+        `${new Date().toLocaleTimeString()}: Polling Outbox table from Order DB`,
+      );
       return this.findUnsentEvents();
     }, this.pollingInterval);
   }
 
   async findUnsentEvents() {
-    const unSent = await this.outboxRepository.findAll<Outbox>({ where: { sent: 0 } });
+    const unSent = await this.outboxRepository.findAll<Outbox>({
+      where: { sent: 0 },
+    });
     const count = unSent.length;
-    let sent = 0;
-    console.log(`---------- Sent ${sent}/${count} ---------`);
+    let sent = 1;
+    console.log(`Messages to send: ${count}`);
     unSent.forEach((e: Outbox) => {
-      if (e.getDataValue("type") === PENDING) {
+      if (e.getDataValue('type') === PENDING) {
         const orderCreatedEvent: OrderEvent = {
           type: OrderEventsTypes.ORDER_CREATED,
-          orderId: e.getDataValue("orderId"),
-          customerId: e.getDataValue("customerId"),
-          orderTotal: e.getDataValue("orderTotal")
+          orderId: e.getDataValue('orderId'),
+          customerId: e.getDataValue('customerId'),
+          orderTotal: e.getDataValue('orderTotal'),
         };
+        console.log(`---------- Sent ${sent}/${count} ---------`);
         this.eventPublisher.publish(orderCreatedEvent);
         sent++;
-        console.log(`---------- Sent ${sent}/${count} ---------`);
-        this.outboxRepository.update({ sent: 1 }, { where: { id: e.getDataValue("id") } });
+        this.outboxRepository.update(
+          { sent: 1 },
+          { where: { id: e.getDataValue('id') } },
+        );
       }
     });
-
-
   }
 }
